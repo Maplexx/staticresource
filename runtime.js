ce53262bd478a655c0d3e78caad95fa53545c312
Maplexx <56779833+Maplexx@users.noreply.github.com>
2023-11-26 12:06:28 +0800

    Update runtime.js

--- runtime.js
+++ runtime.js
@@ -17,7 +17,7 @@ function createtime() {
   (currentTimeHtml =
     hnum < 18 && hnum >= 9
       ? `<img class='boardsign' src='https://img.shields.io/badge/%E4%B8%8A%E7%8F%AD%E6%97%B6%E9%97%B4-%E7%B4%A7%E5%BC%A0%E5%9C%B0%E6%91%B8%E9%B1%BC%E4%B8%AD-orange' title='maplex正在紧张的摸鱼中'><span class='textTip'> <br> 本站已经运行了 ${dnum} 天</span><span id='runtime'> ${hnum} 小时 ${mnum} 分 ${snum} 秒 </span> <i class='fas fa-heartbeat' style='color:red'></i>`
-    : `<img class='boardsign'  src='https://img.shields.io/badge/%E4%B8%8B%E7%8F%AD%E6%97%B6%E9%97%B4-%E5%A4%A7%E8%83%86%E5%BE%97%E6%91%B8%E9%B1%BC%E4%B8%AD-green' title='maplex正在大胆的摸鱼'><span class='textTip'> <br> 本站已经运行了 ${dnum} 天</span><span id='runtime'> ${hnum} 小时 ${mnum} 分 ${snum} 秒 </span> <i class='fas fa-heartbeat' style='color:red'></i>`),
+    : `<img class='boardsign'  src='https://img.shields.io/badge/%E4%B8%8B%E7%8F%AD%E6%97%B6%E9%97%B4-%E5%A4%A7%E8%83%86%E5%9C%B0%E6%91%B8%E9%B1%BC%E4%B8%AD-green' title='maplex正在大胆的摸鱼'><span class='textTip'> <br> 本站已经运行了 ${dnum} 天</span><span id='runtime'> ${hnum} 小时 ${mnum} 分 ${snum} 秒 </span> <i class='fas fa-heartbeat' style='color:red'></i>`),
     document.getElementById("workboard") && (document.getElementById("workboard").innerHTML = currentTimeHtml);
 }
 setInterval(() => {
